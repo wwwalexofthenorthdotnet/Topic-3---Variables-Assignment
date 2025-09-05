9c236011063a00b3c03a05729b66c881da0ed930
Alexander Ronan <alexofthenorth@protonmail.com>
2025-09-05 11:57:38 -0400

    Added circle area finder

--- Topic 3 - Variables Assignment/Program.cs
+++ Topic 3 - Variables Assignment/Program.cs
@@ -4,17 +4,31 @@
     {
         static void Main(string[] args)
         {
-            int roomNumber;
-            double coursePrice;
-            string courseName;
+            int roomNumber, graduationYear, cicleRadius;
+            double coursePrice, pi;
+            string courseName, firstName, lastName;
 
             roomNumber = 29;
             coursePrice = 2.99;
             courseName = "Computer Science";
+            firstName = "Alex";
+            lastName = "Ronan";
+            graduationYear = 2027;
+            cicleRadius = 15; //cm
+            pi = 3.14159;
 
+            //This diplays the course information
             Console.WriteLine("This is room #" + roomNumber);
-            Console.WriteLine("The price is " + coursePrice .ToString("C"));
+            Console.WriteLine("The price is " + coursePrice.ToString("C"));
             Console.WriteLine("I'm learning a bit of " + courseName);
+
+            //This displays the student information
+            Console.WriteLine("My name is " + firstName + " " + lastName + ", and I'll graduate in " + graduationYear);
+
+            //This finds the area of a circle
+            Console.WriteLine("The area of your circle is: " + (pi * Math.Pow(cicleRadius, 2)) .ToString("0.0"));
+
+            //Do pythagorean theorem or something
         }
     }
 }
