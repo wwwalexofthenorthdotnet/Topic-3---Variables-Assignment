1ffeb9ef954a02558b8990d257074051626e27f7
Alexander Ronan <alexofthenorth@protonmail.com>
2025-09-05 11:50:23 -0400

    Add project files.

---
+++ Topic 3 - Variables Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Topic_3___Variables_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int roomNumber;
+            double coursePrice;
+            string courseName;
+
+            roomNumber = 29;
+            coursePrice = 2.99;
+            courseName = "Computer Science";
+
+            Console.WriteLine("This is room #" + roomNumber);
+            Console.WriteLine("The price is " + coursePrice .ToString("C"));
+            Console.WriteLine("I'm learning a bit of " + courseName);
+        }
+    }
+}
